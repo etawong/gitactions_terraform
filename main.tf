7e4bfa18fbdefe5d1a70b0388782635ed8acc50c
etawong <etawong@gmail.com>
2023-09-23 15:56:11 -0700

    changed main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 provider "aws" {
   region = "us-west-2"
 }
@@ -15,4 +16,13 @@ resource "aws_instance" "myEC2" {
 
 output "public_IP" {
   value = "aws_instance.myEC2.public_ip"
+}
+*/
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
 }
